6ee151c678b1191b574f08c80df5913201112fe4
Gregg Fine <bbeez8500@gmail.com>
2018-02-07 18:50:13 -0500

    Rough Dual Inputs

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
-const form = document.getElementById("input-form");
+const projectNameForm = document.getElementById("project-name-input-form");
+const instrumentNameForm = document.getElementById("instrument-input-form");
+const projectDetail = document.querySelector(".project-detail");
+const ul = document.querySelector("ul");
 
-form.addEventListener("submit", function(e){
+projectNameForm.addEventListener("submit", function(e){
 	e.preventDefault();
 
 	const projectName = document.getElementById("project-name").value;
-	const instrumentName = document.getElementById("instrument-name").value;
 
-	const instrumentList = [];
+	const h3 = document.createElement("h3");
 
-	instrumentList.push(instrumentName);
-
-	const project = {
-		projectName: projectName,
-		instrumentList: instrumentList
-	};
-
-	const projects = [];
-
-	if(localStorage.getItem("Projects") == null){
-		projects.push(project);
-		localStorage.setItem("Projects", JSON.stringify(projects));
-	} else {
-		const storageProjects = JSON.parse(localStorage.getItem("Projects"));
-		storageProjects.push(project);
-		localStorage.setItem("Projects", JSON.stringify(storageProjects));
-	}
+	console.log(projectName)
+	h3.textContent = projectName;
+	projectDetail.appendChild(h3);
 
 	this.reset();
 
 })
+
+	const instrumentList = [];
+
+instrumentNameForm.addEventListener("submit", function(e){
+	e.preventDefault();
+
+	let instrumentName = document.getElementById("instrument-name").value;
+
+	let li = document.createElement("li");
+	li.textContent = instrumentName;
+	ul.appendChild(li);
+
+	this.reset();
+})
+	
+
+
+// Problem: Each time I input an instrument, the forEach loop
+// creates lis for everything in the Array
+
+//How can I only create an li for an element that hasn't already been created?
+
+
+
+// const project = {
+// 	projectName: projectName,
+// 	instrumentList: instrumentList
+// };
+
+// const projects = [];
+
+// if(localStorage.getItem("Projects") == null){
+// 	projects.push(project);
+// 	localStorage.setItem("Projects", JSON.stringify(projects));
+// } else {
+// 	const storageProjects = JSON.parse(localStorage.getItem("Projects"));
+// 	storageProjects.push(project);
+// 	localStorage.setItem("Projects", JSON.stringify(storageProjects));
+// }
 
 
 /* Need to be able to add more than one instrument */
