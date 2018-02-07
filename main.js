24be1c5ac92669e5779b5165cc5235a52542d6b8
Gregg Fine <bbeez8500@gmail.com>
2018-02-07 14:06:00 -0500

    Basic save to localStorage

--- main.js
+++ main.js
@@ -10,6 +10,25 @@ form.addEventListener("submit", function(e){
 	const projectName = document.getElementById("project-name").value;
 	const instrumentName = document.getElementById("instrument-name").value;
 
+	const project = {
+		projectName: projectName,
+		instrumentName: instrumentName
+	};
+
+	const projects = [];
+
+	projects.push(project);
+
+	localStorage.setItem("Projects", JSON.stringify(projects));
+
 	this.reset();
 
 })
+
+
+/*project = {
+	projectName: projectName,
+	instrumentName: instrumentName
+} */
+
+//localStorage.setItem("Projects", object)
