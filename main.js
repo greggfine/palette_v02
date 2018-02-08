20ea5cb4c2d082f64bc35238203814b47ee2429e
Gregg Fine <bbeez8500@gmail.com>
2018-02-07 20:17:41 -0500

    Basic LocalStorage Working

--- main.js
+++ main.js
@@ -2,36 +2,86 @@ const projectNameForm = document.getElementById("project-name-input-form");
 const instrumentNameForm = document.getElementById("instrument-input-form");
 const projectDetail = document.querySelector(".project-detail");
 const ul = document.querySelector("ul");
+const doneBtn = document.querySelector("#done-btn");
+const clearBtn = document.querySelector("#clear-btn");
+
+const finalObj = {};
+const instrumentList = [];
 
 projectNameForm.addEventListener("submit", function(e){
 	e.preventDefault();
 
 	const projectName = document.getElementById("project-name").value;
 
-	const h3 = document.createElement("h3");
-
-	console.log(projectName)
-	h3.textContent = projectName;
-	projectDetail.appendChild(h3);
+	finalObj.projectName = projectName;
 
 	this.reset();
 
 })
-
-	const instrumentList = [];
 
 instrumentNameForm.addEventListener("submit", function(e){
 	e.preventDefault();
 
 	let instrumentName = document.getElementById("instrument-name").value;
 
-	let li = document.createElement("li");
-	li.textContent = instrumentName;
-	ul.appendChild(li);
+	if(!instrumentList.includes(instrumentName)){
+		instrumentList.push(instrumentName);
+	} 
+
+	finalObj.instrumentList = instrumentList;
+
 
 	this.reset();
 })
+
+doneBtn.addEventListener("click", function(){
+	localStorage.setItem("Projects", JSON.stringify(finalObj));
+
+	var parsed = JSON.parse(localStorage.getItem("Projects"));
+
+	const h3 = document.createElement("h3");
+
+	h3.textContent = parsed.projectName;
+	projectDetail.appendChild(h3);
+
+	// console.log(parsed)
+	parsed.instrumentList.forEach(instrument => {
+		let li = document.createElement("li");
+		li.textContent = instrument;
+		ul.appendChild(li);
+	})
+
+
+})
+
+clearBtn.addEventListener("click", function(){
+	localStorage.clear();
+})
+
+// function grabProject(){
+
+// 	var parsed = JSON.parse(localStorage.getItem("Projects"));
+// 	console.log(parsed)
+// }
 	
+
+/*
+
+// const h3 = document.createElement("h3");
+
+// h3.textContent = projectName;
+// projectDetail.appendChild(h3);
+
+// localStorage.setItem("Projects", JSON.stringify(projectName));
+
+
+// let li = document.createElement("li");
+// li.textContent = instrumentName;
+// ul.appendChild(li);
+
+// localStorage.setItem("Projects", JSON.stringify(instrumentList));
+
+*/
 
 
 // Problem: Each time I input an instrument, the forEach loop
